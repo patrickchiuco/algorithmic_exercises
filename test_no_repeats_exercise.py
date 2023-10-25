9082a27ca4795fda04a6889ad2f9746e6233fd64
Patrick Michael Chiuco <patrickchiuco@Patricks-MacBook-Pro.local>
2023-10-25 10:28:46 +0800

    Adds comments to tests.

--- test_no_repeats_exercise.py
+++ test_no_repeats_exercise.py
@@ -10,15 +10,21 @@ def test_pairwise():
 	answer = no_repeats('aab')
 	assert type(answer) is int
 
+	# checks that no_repeats returns the expected
+	# answer for the usual case
 	answer = no_repeats('aab')
 	assert answer == 2
 
+	# checks that no_repeats returns 0 when all
+	# characters are the same
 	answer = no_repeats('aaa')
 	assert answer == 0
 
 	answer = no_repeats('aabb')
 	assert answer == 8
 	
+	# checks that no_repeats returns the correct result
+	# when all characters are unique
 	answer = no_repeats('abcdefa')
 	assert answer == 3600
 
@@ -28,6 +34,7 @@ def test_pairwise():
 	answer = no_repeats('zzzzzzzz')
 	assert answer == 0
 
+	# checks for edge case where there is only one character
 	answer = no_repeats('a')
 	assert answer == 1
 
